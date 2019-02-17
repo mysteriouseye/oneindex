de313794401e244a6e8900799a53be39d133f231
mysteriouseye <1597273688@qq.com>
2019-02-17 23:23:49 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,105 @@
+<?php return array (
+  'site_name' => '小凌的Onedrive',
+  'password' => 'Iwantslq1314..$',
+  'style' => 'nexmoe',
+  'onedrive_root' => '/',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 360,
+  'cache_refresh_time' => 600,
+  'root_path' => '',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'zvqFIHMD3857rmcpND7}-#~',
+  'client_id' => '2b16b0f8-a23f-4d9b-a2e8-dbde4f3944b2',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjT3vND-vUfmk4KkgtMN9G219pWrmbFYJYgaK0S-z6Chq-i00XG5cATysW0ChVRLLuyqZGw5zw0mOPKgP1J0ON0cVV9XNUTTQyv4-8FeYGczVjZ-U5UzUBiPZvIP8EwvatELIhmNDl5ymbXFzXKm_QBH55Ds6oPJIugYWNdA8o022zQss5qN537p_ZPab9wdM758zVPJuMniX-DOGL5QhN-dACdNHYloVF2AsVwolSJB8N-tR9WuLnWq9MzNOOaZBalJF45Erem5J0znc8FeRcIrgVIg_aZBaYPDco4VrMIKihHV55hLZ3G1-WqDvjnOHhCPjmtQm_ZFa5irgduFh894TPJ2rubxjDSaGWeAHyhTJ2RbZ8Euiq-AbB01n9TZPwCGaS8Ky6jYJltpvtiAgHy26PucbJMtE-_uHSuMt4G46hiVIX85H1nMQQcMOgH-AYWZqvKXxkR3zdx5qtQJpsOva_nTQ_WpaveSpJkpH2-xuSiiJBV1rN-8DPj4mdctRGKej-RKHcVHFlCQ053KGjDbUEAR69JL9DKdFzCOaydrnVIDxofwx2DQwE7W5msNhBhvg2tUuxLpTw6A8X5-8AUuzF3c6EBDw7GtZa_r2YRHklAKiB5rlTqc18dNoS4-3Xupz6WMPAS5yT2VTAUR3SAUY8HmOpaE961JQ__0MiRpqRh7XVypmJs-AJ27HZpdnvhaJCo0dojATKawfvwwQB8bwamFFqhd9knmeZr5QLf_F5_gDZ4k9X72eoRAppIcdgWafhijc8xbxfNdmugqZDvkiAA',
+  'onedrive_hide' => '',
+);
